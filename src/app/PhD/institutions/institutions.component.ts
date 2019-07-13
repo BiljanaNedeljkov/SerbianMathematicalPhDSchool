a3543494c609a8056a2b98007c57ccd050cea13f
Biljana Nedeljkov <biljanapezos@yahoo.com>
2019-07-13 20:23:26 +0200

    Genereted components

---
+++ src/app/PhD/institutions/institutions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'phd-institutions',
+  templateUrl: './institutions.component.html',
+  styleUrls: ['./institutions.component.scss']
+})
+export class InstitutionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/navbar/navbar.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { HomeComponent } from './PhD/home/home.component';
+import { AboutComponent } from './PhD/about/about.component';
+import { StudentsComponent } from './PhD/students/students.component';
+import { ContactComponent } from './PhD/contact/contact.component';
+import { LecturersComponent } from './PhD/lecturers/lecturers.component';
+import { InstitutionsComponent } from './PhD/institutions/institutions.component';
+import { ModulesComponent } from './PhD/modules/modules.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    StudentsComponent,
+    ContactComponent,
+    LecturersComponent,
+    InstitutionsComponent,
+    ModulesComponent
   ],
   imports: [
     BrowserModule,
