6ebe2f232a43898ce47cafe8aed231cfa593c3ff
Biljana Nedeljkov <biljanapezos@yahoo.com>
2019-09-06 14:21:20 +0200

    Students and useful component finished

--- src/app/PhD/students/students.component.ts
+++ src/app/PhD/students/students.component.ts
@@ -8,11 +8,15 @@ import { NgbModal, NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
   providers: [NgbCarouselConfig]
 })
 export class StudentsComponent {
+  showNavigationArrows = false;
+  showNavigationIndicators = false;
   images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
   closeResult: string;
 
   constructor(private modalService: NgbModal, config: NgbCarouselConfig) {
-    config.interval = 3000;
+    config.showNavigationArrows = false;
+    config.showNavigationIndicators = false;
+    config.interval = 2000;
     config.wrap = true;
     config.keyboard = true;
     config.pauseOnHover = true;
